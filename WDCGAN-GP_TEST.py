b0e84899dfb314ce0269f04ddc5416eff390d63d
YixinChen-AI <61945757+YixinChen-AI@users.noreply.github.com>
2020-03-09 02:39:26 +0800

    Update WDCGAN-GP_TEST.py

--- WDCGAN-GP_TEST.py
+++ WDCGAN-GP_TEST.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import torch
 from torchvision.utils import make_grid
+import numpy as np
 class generator(nn.Module):
     def __init__(self,input_size,num_feature):
         super(generator, self).__init__()
@@ -30,21 +31,13 @@ class generator(nn.Module):
         return x
 G = generator(100,1*56*56)
 if torch.cuda.is_available():
-    G = G.cuda()
+    G = G
 g_optimizer = torch.optim.Adam(G.parameters(), lr=0.0003)
 G.load_state_dict(torch.load('./generator_WDCGAN-GP.pth'))
 z_dimension = 100
-z = torch.randn((1, z_dimension)).cuda()
+z = torch.randn((80, z_dimension))
 fake_img = G(z)
-fake_img = fake_img.view(28,28).detach().cpu()
-fake_img1 = fake_img*0.5+0.5
-# fake_img = transforms.ToPILImage(fake_img)
-plt.imshow(fake_img,cmap='gray')
-plt.axis('off')
+img = make_grid(fake_img,nrow=8).clamp(0,1).detach().numpy()
+plt.imshow(np.transpose(img,(1,2,0)))
 plt.show()
-plt.imshow(fake_img1,cmap='gray')
-plt.axis('off')
-plt.show()
-print(fake_img[0])
-print(fake_img1[0])
 
